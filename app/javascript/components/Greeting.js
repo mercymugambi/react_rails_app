fa5160bcdc81e2d22b43c7aefff81a80de8dacca
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-15 17:24:50 +0300

    Fix the fetching api

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -2,17 +2,21 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchGreetings } from "../redux/greetings/greetingSlice";
+
 const Greeting = () => {
   const dispatch = useDispatch();
+
   const { greetings, isLoading, error } = useSelector( (state) => state.greetings);
+
   useEffect(() => {
     dispatch(fetchGreetings());
   }, [dispatch]);
+
   return (
     <div>
-      {console.log(greetings.data)}
       {greetings && <h2>{greetings.data}</h2>}
     </div>
   );
 };
+
 export default Greeting;
