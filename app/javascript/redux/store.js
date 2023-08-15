b8549e0686826d4c80533ba51167fd0009565af2
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-15 16:30:23 +0300

    Configure store

---
+++ app/javascript/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import greetingReducer from "./greetings/greetingSlice";
+const store = configureStore({
+  reducer: {
+    greetings: greetingReducer,
+  },
+});
+export default store;
